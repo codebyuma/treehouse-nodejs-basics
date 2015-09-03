de3e62a26095fb5a0f2f81e39ded5402e8f8345c
Uma Chandran <umachandran6@gmail.com>
2015-09-03 16:31:50 -0400

    Add updated app.js which includes comments

--- CheckForecast/app.js
+++ CheckForecast/app.js
@@ -1,7 +1,9 @@
+// Using the Google geocode API and the forecast.io API, take in a postal code and display the current weather and forecast for that location
+
 
 var getForecast = require("./forecast.js"); // .js at the end is optional. Path is mandatory.
 
-var postal = process.argv.slice(2,3);
+var postal = process.argv.slice(2,3); // the postal code entered
 
 
-getForecast.getLocation(postal);
+getForecast.getLocation(postal); // call getLocation in forecast.js
